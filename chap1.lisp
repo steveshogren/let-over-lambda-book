6a535d06946a777d713b1e48e7971f9659ff03a6
Steve Shogren <steve.a.shogren@gmail.com>
2013-04-07 15:52:08 -0400

    chapter 1

---
+++ chap1.lisp
@@ -0,0 +1,38 @@
+(defun mkstr (&rest args)
+  (with-output-to-string (s)
+                         (do-list (a args) (princ a s))))
+
+(defun symb (&rest args)
+  (values (intern (apply #'mkstr args))))
+
+(defun group (source n)
+  (if (zerop n) (error "zero length"))
+  (labels ((rec (source acc)
+                (let ((rest (nthcdr n source)))
+                  (if (consp rest)
+                      (rec rest (cons
+                                 (subseq source 0 n)
+                                 acc))
+                    (nreverse
+                     (cons source acc))))))
+    (if source (rec source nil) nil)))
+
+(defun flatten (x)
+  (labels ((rec (x acc)
+                (cond ((null x) acc)
+                      ((atom x) (cons x acc))
+                      (t (rec
+                          (car x)
+                          (rec (cdr x) acc))))))
+    (rec x nil)))
+
+(defun fact (x)
+  (if (= x 0)
+      1
+    (* x (fact (- x 1)))))
+;;(fact 4) 
+(defun choose (n r)
+  (/ (fact n)
+     (fact (- n r))
+     (fact r)))
+
