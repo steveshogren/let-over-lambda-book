c8a5df51db482ea335712fde6601be5a2ff647c6
Steve Shogren <steve.a.shogren@gmail.com>
2013-04-24 20:26:42 -0400

    adding nlet-tail

--- chap5.lisp
+++ chap5.lisp
@@ -172,4 +172,22 @@
  (and (numberp x) (evenp x))
  'even-number)
 
+(defmacro! nlet-tail (n letargs &rest body)
+  (let ((gs (loop for i in letargs
+                  collect (gensym))))
+    `(macrolet
+         ((,n ,gs
+              `(progn
+                 (psetq
+                  ,@(apply #'nconc
+                           (mapcar
+                            #'list
+                            ',(mapcar #'car letargs)
+                            (list ,@gs))))
+                 (go ,',g!n))))
+       (block ,g!b
+         (let ,letargs
+           (tagbody
+            ,g!n (return-from
+                     ,g!b (progn ,@body))))))))
 
