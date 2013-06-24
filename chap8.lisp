c2b8c23b87c8bcdd22b0a249958a2f6f9e3344d2
Steve Shogren <steve.a.shogren@gmail.com>
2013-06-24 16:55:55 -0400

    Add the prim forms and inner interp macro for forth:

--- chap8.lisp
+++ chap8.lisp
@@ -1,4 +1,39 @@
 (defvar forth-registers
   '(pstack rstack pc
            dict compiling dtable))
+(defstruct forth-word
+  name prev immediate thread)
+
+(defun forth-lookup (w last)
+  (if last
+      (if (eql (forth-word-name last) w)
+          last
+        (forth-lookup
+         w (forth-word-prev last)))))
+
+(defmacro forth-inner-interpreter ()
+  `(loop
+    do (cond
+        ((functionp (car pc))
+         (funcall (car pc)))
+        ((consp (car pc))
+         (push (cdr pc) rstack)
+         (setf pc (car pc)))
+        ((null pc)
+         (setf pc (pop rstack)))
+        (t
+         (push (car pc) pstack)
+         (setf pc (cdr pc))))
+    until (and (null pc) (null rstack))))
+
+;; Prim-form: (name immediate . forms)
+(defvar forth-prim-forms nil)
+
+(defmacro def-forth-naked-prim (&rest code)
+  `(push ',code forth-prim-forms))
+
+(defmacro def-forth-prim (&rest code)
+  `(def-forth-naked-prim
+     ,@code
+     (setf pc (cdr pc))))
 
