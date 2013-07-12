25f3837de8cc08470c078d6c32d4413b19e5faa2
Steve Shogren <steve.a.shogren@gmail.com>
2013-07-12 16:14:13 -0400

    Adding the handle not found function

--- chap8.lisp
+++ chap8.lisp
@@ -135,4 +135,22 @@
        (setf pc (list (forth-word-thread word)))
        (forth-inner-interpreter))))
 
+(defmacro forth-handle-not-found ()
+  `(cond
+    ((and (consp v) (eq (car v) 'quote))
+     (if compiling
+         (forth-compile-in (cadr v))
+       (push (cadr v) pstack)))
+    ((and (consp v) (eq (car v) 'postpone))
+     (let ((word (forth-lookup (cadr v) dict)))
+       (if (not word)
+           (error "Postpone failed: ~a" (cadr v)))
+       (forth-compile-in (forth-word-thread word))))
+    ((symbolp v)
+     (error "Word ~a not found" v))
+    (t
+     (if compiling
+         (forth-compile-in v)
+       (push v pstack)))))
+
 
